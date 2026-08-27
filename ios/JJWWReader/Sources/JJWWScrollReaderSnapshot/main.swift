08499bf36f91ffc0770ba1c90974c8feeb54778a
huntarfischer <huntarfischer@gmail.com>
2026-08-27 00:43:33 -0500

    Render continuous Stage 4 scroll ribbon

--- ios/JJWWReader/Sources/JJWWScrollReaderSnapshot/main.swift
+++ ios/JJWWReader/Sources/JJWWScrollReaderSnapshot/main.swift
@@ -19,11 +19,26 @@ struct ScrollReaderSnapshotCommand {
             canonicalURL: URL(fileURLWithPath: canonicalPath)
         )
         let materialStore = try MaterialProfileStore.bundled()
-        let view = ScrollReaderGateSheet(
-            edition: edition,
-            materialStore: materialStore
+
+        try render(
+            ScrollReaderGateSheet(edition: edition, materialStore: materialStore),
+            to: output
         )
 
+        let outputURL = URL(fileURLWithPath: output)
+        let ribbonURL = outputURL
+            .deletingLastPathComponent()
+            .appendingPathComponent("scroll-reader-ribbon-stage4.png")
+        try render(
+            ScrollReaderRibbonSheet(edition: edition, materialStore: materialStore),
+            to: ribbonURL.path
+        )
+
+        print(output)
+        print(ribbonURL.path)
+    }
+
+    private static func render<Content: View>(_ view: Content, to output: String) throws {
         let renderer = ImageRenderer(content: view)
         renderer.scale = 1
         guard let image = renderer.nsImage,
@@ -32,9 +47,7 @@ struct ScrollReaderSnapshotCommand {
               let png = rep.representation(using: .png, properties: [:]) else {
             throw NSError(domain: "JJWWScrollReaderSnapshot", code: 1)
         }
-
         try png.write(to: URL(fileURLWithPath: output))
-        print(output)
     }
 }
 #else
